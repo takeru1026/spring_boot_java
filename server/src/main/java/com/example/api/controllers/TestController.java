1a6d5091f5234768f1c1b9facc303032ff7edb57
takeru1026 <68760801+takeru1026@users.noreply.github.com>
2022-04-14 22:45:18 +0900

    :tada:簡単なWeb API を作ってみた

---
+++ server/src/main/java/com/example/api/controllers/TestController.java
@@ -0,0 +1,35 @@
+package com.example.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // JsonやXML等を返すWebAPI用のコントローラで使用する。アノテーションと言われるやつ
+public class TestController {
+  @RequestMapping(path = "/test", method = RequestMethod.GET) // @RequestMapping:Spring MVC のコントローラに付与して、リクエスト URL に対して、どのメソッドが処理を実行するか定義するアノテーション
+  public String test() {
+    return "ok from test.";
+  }
+
+  @RequestMapping(path = "/test/json", method = RequestMethod.GET)
+  public Example testJson() {
+    var examplePOJO = new Example();
+    examplePOJO.value1 = "foo";
+    examplePOJO.value2 = "bar";
+    return examplePOJO;
+  }
+
+  @RequestMapping(path = "/test/post", method = RequestMethod.POST)
+  public Example testPost(@RequestBody Example requestPOJO) {
+    requestPOJO.value1 = requestPOJO.value1 + ":AddServer";
+    requestPOJO.value2 = requestPOJO.value2 + ":AddServer";
+    return requestPOJO;
+  }
+
+  // static class で宣言しないと @RequestBody で受け取れない……？
+  public static class Example {
+    public String value1;
+    public String value2;
+  }
+}
